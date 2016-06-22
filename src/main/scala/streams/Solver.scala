8b41039c7a6638f6a7a3934b377eea66aa626804
Tom Lous <tomlous@gmail.com>
2016-06-22 10:41:00 +0200

    Fixed neighborsWithHistory & newNeighborsOnly

--- src/main/scala/streams/Solver.scala
+++ src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.isStanding && b.b1 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,9 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    b.legalNeighbors.map({case (block, move) => (block, move :: history)}).toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +38,11 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    neighbors.filter({
+      case (block, move) => !explored.contains(block)
+    })
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
--- src/test/scala/streams/BloxorzSuite.scala
+++ src/test/scala/streams/BloxorzSuite.scala
@@ -62,6 +62,37 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighborsWithHistory") {
+    new Level1 {
+      val resSet = neighborsWithHistory(Block(Pos(1, 1), Pos(1, 1)), List(Left, Up)).toSet
+      val testSet = Set(
+        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      )
+
+      assert(resSet == testSet)
+    }
+  }
+
+  test("newNeighborsOnly") {
+    new Level1 {
+      val resSet = newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      ).toSet
+
+      val testSet =Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      )
+
+      assert(resSet == testSet)
+    }
+  }
+
 
 	test("optimal solution for level 1") {
     new Level1 {
