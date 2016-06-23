6b5d3f6fd816250ed576f0fd53d9b7d5ffc5f4dc
Tom Lous <tomlous@gmail.com>
2016-06-23 16:28:05 +0200

    Fixed Polynominal

--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,32 @@
 package calculator
 
+import Math._
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    //    Δ = b² - 4ac
+    Signal {
+      pow(b(), 2) - 4 * a() * c()
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    //    (-b ± √Δ) / 2a
+    Signal{
+      val d = delta()
+      if(d < 0) Set()
+      else {
+        val bVal = b()
+        val aVal = a()
+
+        Set(
+          (-bVal + sqrt(d)) / (2 * aVal),
+          (-bVal - sqrt(d)) / (2 * aVal)
+        )
+      }
+
+    }
   }
 }
